9be2d6d27b209850fe7ca897995f97d53a3d65a7
Josh Long <josh@joshlong.com>
2020-11-29 05:04:15 -0800

    polish

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-echo "\$1: [$1]"
-java -jar waiter.jar $1 && echo ::set-output name=success::true || echo ::set-output name=success::false
+echo "trying $1 "
+java -jar waiter.jar "$1" && echo ::set-output name=success::true || echo ::set-output name=success::false
 echo ::set-output name=time::$(date)
