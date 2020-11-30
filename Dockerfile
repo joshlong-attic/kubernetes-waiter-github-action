8a4bed23eb754edebb7905d280a3c656ed0d438d
Josh Long <josh@joshlong.com>
2020-11-29 18:11:35 -0800

    polish

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY . /work
 COPY entrypoint.sh /work/entrypoint.sh
 COPY waiter.jar  /work/waiter.jar
 WORKDIR /work
-CMD [  "/work/entrypoint.sh"]
+CMD ["/work/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+cat ~/.kube/config | grep -v token  | grep -v certifi
+
 env
+
+cd $GITHUB_WORKSPACE && ls -la && find .
+
 java -Dwaiter.application-names="$INPUT_WAITLIST" -jar /work/waiter.jar   && echo ::set-output name=success::true || echo ::set-output name=success::false
 echo ::set-output name=time::$(date)
