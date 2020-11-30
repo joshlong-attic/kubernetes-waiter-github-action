b95399af5013e1c9f93257baa3aa6dabdb10d097
Josh Long <josh@joshlong.com>
2020-11-29 17:38:10 -0800

    polish

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-
+env
 java -Dwaiter.application-names="$INPUT_WAITLIST" -jar /work/waiter.jar   && echo ::set-output name=success::true || echo ::set-output name=success::false
 echo ::set-output name=time::$(date)
